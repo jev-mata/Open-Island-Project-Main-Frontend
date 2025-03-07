5dc8c25e42c50f2b3cd547e663c5ff9845838909
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-03-07 17:27:16 +0800

    With Save Island and login and signin aleardy connected

--- src/app/lib/Axios_Open.tsx
+++ src/app/lib/Axios_Open.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios_Open = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://cms.openisland.ph",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://www.openisland.ph",
   withCredentials: true, // ✅ Needed for CSRF and authentication
 });
 
