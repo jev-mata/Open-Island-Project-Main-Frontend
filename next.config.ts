8a1e82332a92f39436a1b2bd5d44d033b567871b
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-03-26 17:14:10 +0800

    Profile Improvements and Display

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 's3-bucket-open-island.s3.ap-southeast-1.amazonaws.com'],
   }, 
